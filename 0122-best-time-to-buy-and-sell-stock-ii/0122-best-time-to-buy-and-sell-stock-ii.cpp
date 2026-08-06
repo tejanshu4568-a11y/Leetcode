351c72d25862a82e573ce2434d02646f938283ba
Tejanshu Ranjha <tejanshu4568@gmail.com>
2026-08-06 09:41:43 +0530

    Time: 3 ms (42.87%), Space: 20.1 MB (47.18%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices) {
+        int totalProfit = 0;
+        for (int i = 1; i < prices.size(); i++) {
+            if (prices[i] > prices[i - 1]) {
+                totalProfit += (prices[i] - prices[i - 1]);
+            }
+        }
+        return totalProfit;
+    }
+};
