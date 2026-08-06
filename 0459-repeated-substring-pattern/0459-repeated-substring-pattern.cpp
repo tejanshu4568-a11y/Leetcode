3c2b7127a912385db674530698e088bfe6a381d2
Tejanshu Ranjha <tejanshu4568@gmail.com>
2026-08-06 09:49:10 +0530

    Time: 4 ms (76.61%), Space: 15.3 MB (59.76%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,8 @@
+#include <string>
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string doubled = s + s;
+        return doubled.substr(1, doubled.size() - 2).find(s) != string::npos;
+    }
+};
