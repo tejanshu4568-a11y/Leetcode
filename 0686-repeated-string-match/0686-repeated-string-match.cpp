3873f46ab273babaf0a4bdae04cdbb70d8f216c2
Tejanshu Ranjha <tejanshu4568@gmail.com>
2026-09-08 11:51:24 +0530

    Time: 3 ms (77.01%), Space: 9.8 MB (45.49%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -0,0 +1,21 @@
+#include <string>
+
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        string s = "";
+        int count = 0;
+        
+        while (s.length() < b.length()) {
+            s += a;
+            count++;
+        }
+        
+        if (s.find(b) != string::npos) return count;
+        
+        s += a;
+        if (s.find(b) != string::npos) return count + 1;
+        
+        return -1;
+    }
+};
